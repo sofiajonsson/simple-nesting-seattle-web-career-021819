e24a08de08e7f0ca30466097dede4b27744ee3b2
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-15 20:47:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-adding_matz[:programmer_hash]= "yukihiro_matsumoto"
+adding_matz[programmer_hash]= [:yukihiro_matsumoto]
 end
 
 def changing_alan
